825117d00b7a90aa04305ca3527fcfd7901fc234
jylei <jlei91@gmail.com>
2018-01-23 00:59:00 -0800

    cors

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    if (req.method === 'OPTIONS') {
+      return res.send(204);
+    }
+    next();
+  });
+
 app.use('/lol', championRouter);
 
 app.get('/champion', (req, res) => {
